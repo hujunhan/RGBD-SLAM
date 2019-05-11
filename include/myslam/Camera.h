e298227354fe1ef2fb87d5f5040158f3100a1404
Junhan Hu <3160102202@zju.edu.cn>
2019-05-12 00:56:21 +0800

    initial
    First commit
    initialize this repo with typical cpp-sytle project

---
+++ include/myslam/Camera.h
@@ -0,0 +1,20 @@
+//
+// Created by hu on 2019/5/9.
+//
+
+#ifndef RGBD_SLAM_CAMERA_H
+#define RGBD_SLAM_CAMERA_H
+
+#include "common_include.h"
+
+namespace myslam {
+    class Camera {
+        typedef std::shared_ptr<Camera> Ptr;
+        Camera();
+
+
+
+    };
+
+}
+#endif //RGBD_SLAM_CAMERA_H
---
+++ include/myslam/common_include.h
@@ -0,0 +1,10 @@
+//
+// Created by hu on 2019/5/9.
+//
+
+#ifndef RGBD_SLAM_COMMON_INCLUDE_H
+#define RGBD_SLAM_COMMON_INCLUDE_H
+using namespace std;
+
+#include "memory"
+#endif //RGBD_SLAM_COMMON_INCLUDE_H
